8418f80de8ac04d997da6f3ba94df48294554df0
martinasenov <martin.manuilov.asenov@gmail.com>
2024-04-16 02:41:23 +0200

    Project Create page - save and delete functions added

--- src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,39 +1,44 @@
 package com.cydeo.bootstrap;
 
+import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Gender;
+import com.cydeo.enums.Status;
+import com.cydeo.service.ProjectService;
 import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
+
 @Component
 public class DataGenerator implements CommandLineRunner {
 
+
     private final RoleService roleService;
     private final UserService userService;
+    private final ProjectService projectService;
 
-    public DataGenerator(RoleService roleService, UserService userService) {
+    public DataGenerator(RoleService roleService, UserService userService, ProjectService projectService) {
         this.roleService = roleService;
-        this.userService=userService;
-
+        this.userService = userService;
+        this.projectService = projectService;
     }
 
-
-    //this method will be executed first before everything when we  start our app
     @Override
     public void run(String... args) throws Exception {
 
-        RoleDTO adminRole=new RoleDTO(1L,"Admin");
-        RoleDTO managerRole=new RoleDTO(2L,"Manager");
-        RoleDTO employeeRole=new RoleDTO(3L,"Employee");
+        RoleDTO adminRole = new RoleDTO(1L,"Admin");
+        RoleDTO managerRole = new RoleDTO(2L,"Manager");
+        RoleDTO employeeRole = new RoleDTO(3L,"Employee");
 
 
         roleService.save(adminRole);
         roleService.save(managerRole);
         roleService.save(employeeRole);
-
 
         UserDTO user1 = new UserDTO("John", "Kesy",
                 "john@cydeo.com", "Abc1", true, "7459684532", managerRole, Gender.MALE);
@@ -45,7 +50,7 @@ public class DataGenerator implements CommandLineRunner {
                 "craig@cydeo.com", "Abc3", true, "7777775566", employeeRole, Gender.MALE);
         UserDTO user4 = new UserDTO("Shaun",
                 "Hayns", "shaun@cydeo.com", "Abc4", true, "3256987412", managerRole, Gender.MALE);
-        UserDTO user6 = new UserDTO("Elizabeth",
+        UserDTO user6 = new UserDTO("Elizebeth",
                 "Loren", "elizebeth@cydeo.com", "Abc4", true, "5306987412", employeeRole, Gender.FEMALE);
         UserDTO user7 = new UserDTO("Maria",
                 "Ada", "maria@cydeo.com", "Abc4", true, "9996987412", employeeRole, Gender.FEMALE);
@@ -60,7 +65,16 @@ public class DataGenerator implements CommandLineRunner {
         userService.save(user6);
         userService.save(user7);
         userService.save(user8);
+
+        ProjectDTO project1 = new ProjectDTO("Spring MVC", "PR001", user1, LocalDate.now(), LocalDate.now().plusDays(25), "Creating Controllers", Status.OPEN);
+        ProjectDTO project2 = new ProjectDTO("Spring ORM", "PR002", user2, LocalDate.now(), LocalDate.now().plusDays(10), "Creating Database", Status.IN_PROGRESS);
+        ProjectDTO project3 = new ProjectDTO("Spring Container", "PR003", user1, LocalDate.now(), LocalDate.now().plusDays(32), "Creating Container", Status.IN_PROGRESS);
+
+        projectService.save(project1);
+        projectService.save(project2);
+        projectService.save(project3);
+
+
+
     }
-
-
 }
---
+++ src/main/java/com/cydeo/converter/UserDtoConverter.java
@@ -0,0 +1,24 @@
+package com.cydeo.converter;
+
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.UserService;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDtoConverter  implements Converter<String, UserDTO> {
+
+    UserService userService;
+
+    public UserDtoConverter(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDTO convert(String source) {
+        return userService.findById(source);
+    }
+}
